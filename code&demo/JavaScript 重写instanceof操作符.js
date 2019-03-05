1d0dc7bf8f9a2c0a3e988e1fb1c074a5111be9c4
TanYJie <40588695+TanYJie@users.noreply.github.com>
2019-03-05 14:09:46 +0800

    Update JavaScript 重写instanceof操作符.js

--- code&demo/JavaScript 重写instanceof操作符.js
+++ code&demo/JavaScript 重写instanceof操作符.js
@@ -12,22 +12,22 @@ function myInstanceof(obj,category){
 
 //测试代码
 //基本类型值检测
-console.log(myInstanceof(1,Number));
-console.log(myInstanceof("a",String));
-console.log(myInstanceof(true,Boolean));
+console.log(myInstanceof(1,Number)); //true
+console.log(myInstanceof("a",String)); //true
+console.log(myInstanceof(true,Boolean)); //true
 var id = Symbol("id"); 
-console.log(myInstanceof(id,Symbol));
+console.log(myInstanceof(id,Symbol)); //true
 
 //Date,RegExp,Function类型检测
 var date = new Date();
-console.log(myInstanceof(date,Date));
+console.log(myInstanceof(date,Date)); //true
 var reg = /at/g;
-console.log(myInstanceof(reg,RegExp));
-console.log(myInstanceof(myInstanceof,Function));
+console.log(myInstanceof(reg,RegExp)); //true
+console.log(myInstanceof(myInstanceof,Function)); //true
 
 //Math,window内置对象检测
-console.log(myInstanceof(window,Window));
-console.log(myInstanceof(Math,Math.__proto__.constructor));
+console.log(myInstanceof(window,Window)); //true
+console.log(myInstanceof(Math,Math.__proto__.constructor)); //true
 
 //自定义类型检测
 function Person(name,age){
@@ -35,4 +35,10 @@ function Person(name,age){
     this.age = age;
 }
 var person1 = new Person("Nicholas",29);
-console.log(myInstanceof(person1,Person));
+console.log(myInstanceof(person1,Person)); //true
+console.log(myInstanceof(person1,Object)); //true
+
+//数组类型检测
+var arr=[];
+console.log(myInstanceof(arr,Array));      //true
+console.log(myInstanceof(arr,Object));     //true

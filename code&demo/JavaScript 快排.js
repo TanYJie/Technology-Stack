c2139ae856a77d4a7859f92489090d4e2e56f8d4
TanYJie <40588695+TanYJie@users.noreply.github.com>
2019-03-18 18:05:45 +0800

    Update JavaScript 快排.js

--- code&demo/JavaScript 快排.js
+++ code&demo/JavaScript 快排.js
@@ -1,23 +1,23 @@
 function Partition(arr,low,high){
-	var temp = arr[low];
-	while(low<high){
-		while(low<high&&arr[high]>=temp){
-			--high;
-		}
-		arr[low]=arr[high];
-		while(low<high&&arr[low]<=temp){
-			++low;
-		}
-		arr[high]=arr[low];
+    var temp = arr[low];
+    while(low<high){
+	while(low<high&&arr[high]>=temp){
+	    --high;
 	}
-	arr[low]=temp;
-	return low;
+	arr[low]=arr[high];
+	while(low<high&&arr[low]<=temp){
+            ++low;
+	}
+	arr[high]=arr[low];
+    }
+    arr[low]=temp;
+    return low;
 }
 
 function quickSort(arr,low,high){
-	if(low<high){
-		var key=Partition(arr,low,high);
-		quickSort(arr,low,key-1);	
-		quickSort(arr,key+1,high);
-	}
+    if(low<high){
+	var key=Partition(arr,low,high);
+	quickSort(arr,low,key-1);	
+	quickSort(arr,key+1,high);
+    }
 }

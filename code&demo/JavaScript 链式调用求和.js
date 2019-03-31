cd60129fc80d6d550c732a3814f55bdc2edbc48f
TanYJie <40588695+TanYJie@users.noreply.github.com>
2019-03-31 14:52:41 +0800

    Update JavaScript 链式调用求和.js

--- code&demo/JavaScript 链式调用求和.js
+++ code&demo/JavaScript 链式调用求和.js
@@ -1,20 +1,20 @@
 var sum = function(...arg){
-	var value = 0;
-	for(val of arg){
-		value += val;
-	}
+    var value = 0;
+    for(val of arg){
+	value += val;
+    }
   
-	var result = function(...args){
-		for(val of args){
-			value += val;
-		}
-		return result;
-	}
-  
-	result.value = function(){
-		console.log(value);
-	}
+    var result = function(...args){
+  	for(val of args){
+	    value += val;
+ 	}
 	return result;
+    }
+  
+    result.value = function(){
+	console.log(value);
+    }
+    return result;
 }
 
 sum(2,3,4,5,6)(1)(10,20)(7).value();  //58

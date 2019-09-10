f4f38bdc424ce23ae436ae3d869feb23ef70eb4d
TanYJie <40588695+TanYJie@users.noreply.github.com>
2019-09-10 23:42:49 +0800

    Create JavaScript 二分查找.js

---
+++ code&demo/JavaScript 二分查找.js
@@ -0,0 +1,15 @@
+function binarySearch(arr,low, high, key) {
+    if (low > high){
+        return -1;
+    }
+    var mid = parseInt((high + low) / 2);
+    if(arr[mid] == key){
+        return mid;
+    }else if (arr[mid] > key){
+        high = mid - 1;
+        return binary_search(arr, low, high, key);
+    }else if (arr[mid] < key){
+        low = mid + 1;
+        return binary_search(arr, low, high, key);
+	}
+};

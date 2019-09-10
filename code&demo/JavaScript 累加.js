926025259bbeec183fa03283b317ecf3aa335c07
TanYJie <40588695+TanYJie@users.noreply.github.com>
2019-09-10 23:45:45 +0800

    Create JavaScript 累加.js

---
+++ code&demo/JavaScript 累加.js
@@ -0,0 +1,22 @@
+var sum = function(...arg){
+	var value = 0;
+	for(val of arg){
+		value += val;
+	}
+	var result = function(...args){
+		for(val of args){
+			value += val;
+		}
+		return result;
+	}
+	result.value = function(){
+		console.log(value);
+	}
+	result.toString = function(){
+		return value;
+	}
+	return result;
+}
+
+sum(2,3,4,5,6)(1)(10,20)(7).value();  //58
+console.log(sum(2,3,4,5,6)(1)(10,20)(7));        //58
